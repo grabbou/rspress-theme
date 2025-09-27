a0cb9cc722bd1676fceb1aa10ef5a4442d4ae77c
Mike <grabbou@gmail.com>
2025-09-27 02:05:00 +0200

    Update CTA texts and links for consistency

--- packages/theme/src/plugin/const.ts
+++ packages/theme/src/plugin/const.ts
@@ -9,20 +9,20 @@ export const OVERVIEW_FILTER_NAME_TEXT = '';
 
 export const OUTLINE_CTA_BUTTON_TEXT = "Let's talk";
 export const OUTLINE_CTA_DESCRIPTION =
-  'We help React Native teams enhance speed, responsiveness, and efficiency.';
-export const OUTLINE_CTA_HEADLINE = "Need to boost your app's performance?";
-export const OUTLINE_CTA_LINK = 'https://callstack.com';
+  'We support teams building scalable apps with React and React Native.';
+export const OUTLINE_CTA_HEADLINE = "Need help with React or React Native projects?";
+export const OUTLINE_CTA_LINK = 'https://www.callstack.com/contact?utm_campaign=Open_Source&utm_source=$TITLE&utm_medium=Documentation&utm_content=OUTLINE_LINK;
 
 export const DOC_FOOTER_CTA_BUTTON_TEXT = "Let's talk";
 export const DOC_FOOTER_CTA_HEADLINE =
   'Need React or React Native\nexpertise you can count on?';
-export const DOC_FOOTER_CTA_LINK = 'https://callstack.com';
+export const DOC_FOOTER_CTA_LINK = 'https://www.callstack.com/contact?utm_campaign=Open_Source&utm_source=$TITLE&utm_medium=Documentation&utm_content=DOCS_BANNER_LINK';
 
 export const HOME_BANNER_HEADLINE =
   'Need React or React Native\nexpertise you can count on?';
 export const HOME_BANNER_DESCRIPTION =
   "We've spent years building full-stack, cross-platform apps and solving tough technical challenges.";
 export const HOME_BANNER_BUTTON_TEXT = "Let's talk";
-export const HOME_BANNER_LINK = 'https://callstack.com';
+export const HOME_BANNER_LINK = 'https://www.callstack.com/contact?utm_campaign=Open_Source&utm_source=$TITLE&utm_medium=Documentation&utm_content=HOME_BANNER_LINK';
 
 export const HOME_FOOTER_LINK = 'https://callstack.com';
